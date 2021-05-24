e72fc24c5ad899fc9ccb127329709035fcff19eb
Anthony Grear <tgrear2008@gmail.com>
2021-05-24 22:31:14 +0700

    create function to produce the sequence to be used to walk through the indices of the nested lists

--- google_foobar_level3.py
+++ google_foobar_level3.py
@@ -1,22 +1,43 @@
-target = 0
-sequence = []
-start = 0
-temp = 0
-counter=0
-while counter < 25:
-    counter +=1
-    sequence.append(temp)
-    if temp == start:
-        target +=1
-        if target==5:
-            target-=1
-            start+=1
-            temp=target
-            continue
-        temp=target
+# target = 0
+# sequence = []
+# start = 0
+# temp = 0
+# counter=0
+# while counter < 25:
+#     counter +=1
+#     sequence.append(temp)
+#     if temp == start:
+#         target +=1
+#         if target==5:
+#             target-=1
+#             start+=1
+#             temp=target
+#             continue
+#         temp=target
         
             
-        continue
-    temp -=1
-  
-print(sequence)
+#         continue
+#     temp -=1
+def index_gen(max_val):
+    target = 0
+    sequence = []
+    start = 0
+    temp = 0
+    counter=0
+    while counter < max_val**2:
+        counter +=1
+        sequence.append(temp)
+        if temp == start:
+            target +=1
+            if target==max_val:
+                target-=1
+                start+=1
+                temp=target
+                continue
+            temp=target
+            
+                
+            continue
+        temp -=1
+    return sequence
+print(index_gen(6))
