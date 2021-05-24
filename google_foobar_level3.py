348519ad9c2c8d54c9a57bda89408f9f20c3209c
Anthony Grear <tgrear2008@gmail.com>
2021-05-24 22:08:49 +0700

    create function to produce sequence to use for walking through the list of nested lists

--- google_foobar_level3.py
+++ google_foobar_level3.py
@@ -1,20 +1,41 @@
-target = 0
-sequence = []
-start = 0
-temp = 0
-while temp<5:  
-  sequence.append(temp)
-  if temp==4:
-    target+=1
-    temp =target
-    start =0 
-    continue
-    if target == 5:
-      break    
-  if start==temp:
-    start+=1
-    temp = 0    
-    continue
-  temp+=1
+# target = 0
+# sequence = []
+# start = 0
+# temp = 0
+# while temp<5:  
+#   sequence.append(temp)
+#   if temp==4:
+#     target+=1
+#     temp =target
+#     start =0 
+#     continue
+#     if target == 5:
+#       break    
+#   if start==temp:
+#     start+=1
+#     temp = 0    
+#     continue
+#   temp+=1
   
-print(sequence)
+def nested_list_sequence_generator(parameter):
+    target = 0
+    sequence = []
+    start = 0
+    temp = 0
+    while temp<parameter:  
+        sequence.append(temp)
+        if temp==(parameter-1):
+            target+=1
+            temp =target
+            start =0 
+            continue
+            if target == parameter:
+                break    
+        if start==temp:
+            start+=1
+            temp = 0    
+            continue
+        temp+=1
+    return sequence
+
+print(nested_list_sequence_generator(6))
