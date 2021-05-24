35745c27d0863d045c59e4a257d2b9bad511ab10
Anthony Grear <tgrear2008@gmail.com>
2021-05-24 22:33:25 +0700

    delete lines already converted to function

--- google_foobar_level3.py
+++ google_foobar_level3.py
@@ -1,23 +1,3 @@
-# target = 0
-# sequence = []
-# start = 0
-# temp = 0
-# counter=0
-# while counter < 25:
-#     counter +=1
-#     sequence.append(temp)
-#     if temp == start:
-#         target +=1
-#         if target==5:
-#             target-=1
-#             start+=1
-#             temp=target
-#             continue
-#         temp=target
-        
-            
-#         continue
-#     temp -=1
 def index_gen(max_val):
     target = 0
     sequence = []
@@ -34,10 +14,9 @@ def index_gen(max_val):
                 start+=1
                 temp=target
                 continue
-            temp=target
-            
-                
+            temp=target                
             continue
         temp -=1
     return sequence
+    
 print(index_gen(6))
