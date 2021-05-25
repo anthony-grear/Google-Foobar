8b8763b0656512524617e366233801cfad6a8117
Anthony Grear <tgrear2008@gmail.com>
2021-05-25 09:35:29 +0700

    increase maze border by 1

--- google_foobar_level3.py
+++ google_foobar_level3.py
@@ -8,7 +8,7 @@ test_maze = [[0,1,1,0,0,0],
 
 def maze_wrapper(maze):
     maze_dimension = len(maze[0])
-    maze_border = [-1]*(maze_dimension+1)
+    maze_border = [-1]*(maze_dimension+2)
     for maze_row in maze:
         maze_row.insert(0,-1)
         maze_row.append(-1)
